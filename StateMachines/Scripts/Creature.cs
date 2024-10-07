37a4dd352a160699cee8ad90f734ad44f780b364
Rhys Kelly <rhysmkelly@sky.com>
2024-10-07 10:29:27 +0100

    +Start pos and current pos

---
+++ StateMachines/Scripts/Creature.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachines.Scripts
+{
+    internal class Creature
+    {
+        protected Vector2 StartPos;
+        protected Vector2 CurrentPos;
+
+        public void Start(Vector2 startPos, Vector2 currentPos) 
+        {
+            StartPos = startPos;
+            CurrentPos = currentPos;
+
+        }
+
+        public void UP()
+        {
+            CurrentPos.Y += 1;
+        }
+
+        public void DOWN()
+        {
+            CurrentPos.Y -= 1;
+        }
+
+        public void LEFT()
+        {
+            CurrentPos.X -= 1;
+        }
+
+        public void RIGHT()
+        {
+            CurrentPos.X += 1;
+        }
+
+        public void ResetPos() 
+        {
+            CurrentPos.X = StartPos.X;
+            CurrentPos.Y = StartPos.Y;   
+        }
+
+    }
+}
