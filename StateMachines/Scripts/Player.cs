a87dd96769cde9ca50f266977881a8aac97add47
Rhys Kelly <rhysmkelly@sky.com>
2024-10-07 11:37:15 +0100

    + Player thing

---
+++ StateMachines/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateMachines.Scripts
+{
+    internal class Player : Creature
+    {
+        private int CurrentLives;
+        private int InitialLives;
+        private int Score;
+
+        public Player(Vector2 pos, int lives) : base(pos)
+        {
+            CurrentLives = lives;
+            InitialLives = CurrentLives;
+            Score = 0;
+        }
+
+        public void GetLives(int lives)
+        {
+            return; 
+        }
+
+        public void ReduceLives()
+        {
+            CurrentLives--;
+        }
+
+        public void ResetLives()
+        {
+            if (CurrentLives == 0)
+            {
+                CurrentLives = InitialLives;
+            }
+        }
+
+    }
+}
