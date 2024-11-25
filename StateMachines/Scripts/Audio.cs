c93709ae0c060a6e36c4b30abec8064c7686dd12
Rhys Kelly <rhysmkelly@sky.com>
2024-11-25 11:12:45 +0000

    +Audio file

---
+++ StateMachines/Scripts/Audio.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+using SharpDX.MediaFoundation;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+
+
+namespace StateMachines.Scripts
+{
+    internal class Audio
+    {
+        private List<SoundEffectInstance> effectInstances;
+        private List<Song> song;
+
+        public void LoadContent(ContentManager cm)
+        {
+            LoadContent(cm);
+            List<SoundEffect> sfx = new List<SoundEffect>();
+            sfx.Add(cm.Load<SoundEffect>(@"error3"));
+            sfx.Add(cm.Load<SoundEffect>(@"footstep_concrete_003"));
+            sfx.Add(cm.Load<SoundEffect>(@"footstep08"));
+
+            for (int sfxnum = 0; sfxnum < sfx.Count; sfxnum++)
+            {
+                effectInstances.Add(sfx[sfxnum].CreateInstance());
+            }
+
+            song = new List<Song>();
+            Song gs = cm.Load<Song>("Farm Frolics");
+            song.Add(gs);
+            gs = cm.Load<Song>("Mission Plausible");
+            song.Add(gs);
+            gs = cm.Load<Song>("Sad Descent");
+            song.Add(gs);
+        }
+
+        public void PlaySFX(float vol , int effectNum)
+        {
+            effectInstances[effectNum].Volume = vol;
+            effectInstances[effectNum].Play();
+        }
+
+        public void PlaySong(int trackNum, bool loop)
+        {
+            MediaPlayer.IsRepeating = loop;
+            MediaPlayer.Play(song[trackNum]);
+        }
+
+    }
+}
